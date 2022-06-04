c9592c505ff610773408b307a5416f4d9dd08d9e
Kongo Abdoul <takykongoabdoul@gmail.com>
2022-06-03 18:34:20 -0700

    Add the script 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - bref description
+* Return:0
+*/
+int main(void)
+{
+	char c = 'a';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
