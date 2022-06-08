617b40cc394668ec49f2b9f69dfcdd238bd9a780
Kongo Abdoul <takykongoabdoul@gmail.com>
2022-06-08 08:29:58 -0700

    Add the script 2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10.c - find print 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - find print 10 times the alphabet, in lowercase
  * Return: 0
  */
 void print_alphabet_x10(void)
